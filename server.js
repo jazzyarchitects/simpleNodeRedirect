b548c30b78f3f181a66ce7d21643c810bcbe4037
Jibin Mathews <jibinmathews7@gmail.com>
2016-01-16 14:31:10 +0530

    url correction

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 
 app.all('/', function(req, res, next){
-   res.redirect('http://medicalassistant-remedyshare.rhcloud.com');
+   res.redirect('http://medicalassistant-jazzyarchitect.rhcloud.com/');
 });
 
 app.listen(Number(process.env.PORT || 3001));
